74443550a1ad8a9884ff8e7e7ba0d1c9440e7b1c
jeppesc11 <jeppespanggaard11@gmail.com>
2025-07-25 13:53:06 +0200

    🔨 - Added TestApp

---
+++ TestApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SharePoint.Client;
+using PnP.Framework;
+using PnP.Framework.Provisioning.Model;
+using SharePoint.CSOM.Extensions;
+using System.Net;
+using TestApp.Temp;
+using AuthenticationManager = PnP.Framework.AuthenticationManager;
+
+namespace TestApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string siteUrl = "";
+            string clientId = "";
+            string tenantId = "";
+
+            string certPath = @"";
+            string certPassword = "!";
+
+            string listId = "";
+            string siteId = "";
+
+            var authManager = new AuthenticationManager(clientId, certPath, certPassword, tenantId);
+
+            using var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.SetMinimumLevel(LogLevel.Trace);
+            });
+
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            // Configure global execution logic - now ExecuteScopeAsync gets the actual CSOM setup actions!
+            //SharePoint.CSOM.Extensions.Configuration.CSOMConfiguration.Configure(async (context, setupAction) =>
+            //{
+            //    if (context is ClientContext clientContext)
+            //    {
+            //    }
+            //    else
+            //    {
+            //        // Fallback for non-ClientContext scenarios
+            //        setupAction();
+            //        await context.ExecuteQueryAsync();
+            //    }
+            //});
+
+            using (var context = authManager.GetContext(siteUrl))
+            {
+            }
+        }
+    }
+}
---
+++ TestApp/Temp/TestModel.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.SharePoint.Client;
+using SharePoint.CSOM.Extensions.Extensions;
+using SharePoint.CSOM.Extensions.Models;
+
+namespace TestApp.Temp
+{
+
+    public static class Position
+    {
+        public const string Ceo = "CEO";
+        public const string Developer = "Developer";
+        public const string Nobody = "Nobody";
+    }
+
+    public static class InternalName
+    {
+        public const string Id = "ID";
+        public const string Title = "Title";
+        public const string People = "People";
+        public const string Position = "Position";
+    }
+
+
+
+    public class TestModel : ListItemModel<TestModel>
+    {
+        public string? Title { get; set; }
+        public FieldUserValue? People { get; set; }
+        public string? Position { get; set; }
+
+        public override List<string> GetViewFields()
+        {
+            return new List<string> {
+            InternalName.Title,
+            InternalName.People,
+            InternalName.Position
+        };
+        }
+
+        public override TestModel FromListItem(ListItem item)
+        {
+            if (item == null) return this;
+            this.Id = item.Id;
+            Title = item.FieldExistsAndNotNull(InternalName.Title) ? (string)item[InternalName.Title] : null;
+            People = item.FieldExistsAndNotNull(InternalName.People) ? (FieldUserValue)item[InternalName.People] : null;
+            Position = item.FieldExistsAndNotNull(InternalName.Position) ? (string)item[InternalName.Position] : null;
+            return this;
+        }
+
+        public override Dictionary<string, object?> ToDictionary()
+        {
+            return new Dictionary<string, object?> {
+            { InternalName.Title, Title },
+            { InternalName.People, People },
+            { InternalName.Position, Position }
+        };
+        }
+    }
+}
